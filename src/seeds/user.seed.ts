cc2e3f0be530aab15f1bf9597625d4b930a84227
ArifMubarok <arifrm28@gmail.com>
2025-05-27 21:18:38 +0700

    fix: delete default user and create complex user

--- src/seeds/user.seed.ts
+++ src/seeds/user.seed.ts
@@ -5,7 +5,7 @@ import * as argon2 from 'argon2';
 
 export class UserSeeder implements Seeder {
     async run(dataSource: DataSource): Promise<any> {
-        const users: DeepPartial<User>[] = [
+        const usersToDelete: DeepPartial<User>[] = [
             {
               username: 'admin',
               email: 'admin@gmail.com',
@@ -14,8 +14,39 @@ export class UserSeeder implements Seeder {
               lastName: null,
             },
         ];
-      
-        const newUsers = dataSource.manager.create(User, users);
-        await dataSource.manager.save(newUsers);
+
+        const usersToCreate: DeepPartial<User>[] = [
+            {
+                username: 'admincms',
+                email: 'admincms@gmail.com',
+                password: await argon2.hash('AdminCMSDamdex123!@#'),
+                firstName: 'Admin CMS',
+                lastName: null,
+            },
+        ];
+
+        await Promise.all(usersToDelete.map(async (user) => {
+            const existingUser = await dataSource.manager.findOne(User, {
+                where: {
+                    email: user.email,
+                },
+            });
+
+            if (existingUser) {
+                await dataSource.manager.softDelete(User, existingUser.id);
+            }
+        }))
+
+        await Promise.all(usersToCreate.map(async (user) => {
+            const existingUser = await dataSource.manager.findOne(User, {
+                where: {
+                    email: user.email,
+                },
+            });
+            if (!existingUser) {
+                const newUser = dataSource.manager.create(User, user);
+                await dataSource.manager.save(newUser);
+            }
+        }))
     }
 }
